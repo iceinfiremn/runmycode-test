0b57427d5c8fd76ea5f615f8eb56ae30ba4daf28
iceinfiremn <49744977+iceinfiremn@users.noreply.github.com>
2019-08-18 07:31:37 +0430

    Update ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -1,2 +1,33 @@
-puts "Hello World from Ruby!"
-puts $stdin.read
+// Here You can type your custom JavaScript...
+//gender 4 for mans 1 for female
+const gender=3
+
+jQuery(function($){
+   console.log($('.knightInfoWrapper'))
+  $('.knightInfoWrapper').click()
+  setTimeout(function() {
+         $('.tabsContainer #missions a').click()
+         setInterval(function(){
+           for( let i=0;i< 8; i++){
+      
+            if( $('.knightsList').children().eq(i)){
+           setTimeout(function(){
+            $('.knightsList').children().eq(i).find('a').click()
+            setTimeout(function(){
+              if( $('.tabContentMissionsRewards .header a').length)
+               $('.tabContentMissionsRewards .header a').click()
+              setTimeout(function(){
+                  $('.missionInfo a').click()
+              },1000)
+            },1000)
+              
+        
+           },i*3000)
+          }
+       }
+  }, 20000);
+           
+           
+         }, 3000)
+  
+})
